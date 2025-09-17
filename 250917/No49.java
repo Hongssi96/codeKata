8e7ae15ebd759e4f9be49f0e960d9b11e002eaaf
Hongssi96 <paul960801@gmail.com>
2025-09-17 10:12:21 +0900

    Create No49.java

---
+++ 250917/No49.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] numbers) {
+        Set<Integer> sums = new HashSet<>();
+        for( int i = 0 ; i < numbers.length - 1 ; i++ ){
+            for ( int j = i + 1 ; j < numbers.length ; j++ ){
+                sums.add(numbers[i] + numbers[j]);
+            }
+        }
+        
+        int[] answer = new int[sums.size()];
+        int idx = 0;
+        for ( int sum : sums){
+            answer[idx] = sum;
+            idx++;
+        }
+        Arrays.sort(answer);
+        return answer;
+    }
+}
