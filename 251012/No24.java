2e2b853419d19596ecfaec152d1f2002309a0ebd
Hongssi96 <paul960801@gmail.com>
2025-10-12 12:45:06 +0900

    Create No24.java

---
+++ 251012/No24.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(String[] seoul) {
+        int idx = 0;
+        for (int i = 0 ; i < seoul.length ; i++){
+            String seobang = seoul[i];
+            if (seobang.equals("Kim")){
+                idx = i;
+            }
+        }
+        
+        String answer = "김서방은 " + idx + "에 있다";
+        return answer;
+    }
+}
